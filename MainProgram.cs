84cd01896c4c2ee375d4d3c78fb4229c6197f2e4
Nurhax <iqbalnur2009@gmail.com>
2024-04-18 20:08:57 +0700

    Inisialisasi Tubes Kelompok 5

---
+++ MainProgram.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class MainProgram
+{
+	public static void Main(string[] args)
+	{
+		//Main program dari tubes kita di class ini
+		Console.WriteLine("Testing");
+	}
+}
---
+++ TubesKelompok5/Program.cs
@@ -0,0 +1,8 @@
+﻿public class MainProgram
+{
+    public static void Main(string[] args)
+    {
+        //Disini kita nyatuin semua modul yang telah dibuat ygy
+        Console.WriteLine("Testing");
+    }
+}
