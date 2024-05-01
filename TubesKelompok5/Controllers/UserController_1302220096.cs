b143f27e92de51e93a1db9c3f5522d0b3471c2eb
KAL EL MUHAMMAD PRATAMA <kalpratama@student.telkomuniversity.ac.id>
2024-05-01 21:23:05 +0700

    Menambah API utk register, login belum.

---
+++ TubesKelompok5/Controllers/UserController_1302220096.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TubesKelompok5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController_1302220096 : ControllerBase
+    {
+        private readonly IUser_1302220096 _userService;
+
+        public UserController_1302220096(IUser_1302220096 userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> Register(User_1302220096 model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await _userService.RegisterAsync(model);
+
+            if (!result.Success)
+            {
+                return BadRequest(result.Message);
+            }
+
+            return Ok("Berhasil Mendaftar");
+        }
+    }
+
+}
---
+++ TubesKelompok5/User_1302220096.cs
@@ -0,0 +1,16 @@
+﻿namespace TubesKelompok5
+{
+    public class User_1302220096
+    {
+        public string username { get; set; }
+        public string passwordHashed { get; set; }
+        public string email { get; set; }
+    }
+
+    public class ServiceResponse<T>
+    {
+        public bool Success { get; set; }
+        public string Message { get; set; }
+        public T Data { get; set; }
+    }
+}
