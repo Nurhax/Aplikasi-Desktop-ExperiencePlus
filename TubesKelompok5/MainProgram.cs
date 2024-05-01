a447d2c388ba2694ebf1e83bd15131399c9506fe
hafiiddd <hafid.akhyar0411@gmail.com>
2024-05-01 16:35:30 +0700

    menambahkan method seleksiCV (default main)

--- TubesKelompok5/MainProgram.cs
+++ TubesKelompok5/MainProgram.cs
@@ -5,19 +5,8 @@ public class MainProgram
 {
     public static void Main(string[] args)
     {
-        CVConfig config = new CVConfig();
-        ListCV_1302223050 list = new ListCV_1302223050();
-        ListCV_1302223050.CV list2 = new ListCV_1302223050.CV();
-        if (list2 == null)
-        {
-            config.readConfig();
-            config.writeConfig();
-        }
-        else
-        {
-            config.readConfig();
-            config.rewriteConfig();
-        }
+        //Disini kita nyatuin semua modul yang telah dibuat ygy
+        Console.WriteLine("Testing");
 
     }
 }
