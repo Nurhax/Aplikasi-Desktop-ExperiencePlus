203817f49f9b5a1a81bcff54489e89b65f9582d1
Nurhax <iqbalnur2009@gmail.com>
2024-05-20 15:10:58 +0700

    Inisialisasi GUI Login,Daftar. Menyelesaikan tampilan GUI Login

---
+++ Login/Form1.Designer.cs
@@ -0,0 +1,262 @@
+﻿namespace Login
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            label1 = new Label();
+            panel1 = new Panel();
+            label7 = new Label();
+            label6 = new Label();
+            label5 = new Label();
+            label4 = new Label();
+            textBox2 = new TextBox();
+            textBox1 = new TextBox();
+            button2 = new Button();
+            button1 = new Button();
+            label2 = new Label();
+            panel2 = new Panel();
+            label3 = new Label();
+            label8 = new Label();
+            panel3 = new Panel();
+            panel1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Times New Roman", 39.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(94, -2);
+            label1.Name = "label1";
+            label1.Size = new Size(401, 61);
+            label1.TabIndex = 0;
+            label1.Text = "WELCOME TO";
+            label1.Click += label1_Click;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.Teal;
+            panel1.Controls.Add(panel3);
+            panel1.Controls.Add(label8);
+            panel1.Controls.Add(label7);
+            panel1.Controls.Add(label6);
+            panel1.Controls.Add(label5);
+            panel1.Controls.Add(label4);
+            panel1.Controls.Add(textBox2);
+            panel1.Controls.Add(textBox1);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(label2);
+            panel1.Controls.Add(label1);
+            panel1.ForeColor = SystemColors.ControlText;
+            panel1.Location = new Point(56, 120);
+            panel1.Name = "panel1";
+            panel1.RightToLeft = RightToLeft.No;
+            panel1.Size = new Size(611, 853);
+            panel1.TabIndex = 1;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.SpringGreen;
+            label7.Location = new Point(377, 617);
+            label7.Name = "label7";
+            label7.Size = new Size(150, 31);
+            label7.TabIndex = 9;
+            label7.Text = "Click Here!";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(144, 617);
+            label6.Name = "label6";
+            label6.Size = new Size(227, 31);
+            label6.TabIndex = 8;
+            label6.Text = "Forgot Password?";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.SpringGreen;
+            label5.Location = new Point(377, 572);
+            label5.Name = "label5";
+            label5.Size = new Size(180, 31);
+            label5.TabIndex = 7;
+            label5.Text = "Sign Up Here!";
+            label5.Click += label5_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(67, 572);
+            label4.Name = "label4";
+            label4.Size = new Size(304, 31);
+            label4.TabIndex = 6;
+            label4.Text = "Don't Have An Account?";
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Times New Roman", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox2.Location = new Point(39, 392);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.PlaceholderText = "Password";
+            textBox2.Size = new Size(526, 48);
+            textBox2.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Times New Roman", 24F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox1.Location = new Point(39, 301);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.PlaceholderText = "Username";
+            textBox1.Size = new Size(526, 48);
+            textBox1.TabIndex = 4;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.DarkSlateGray;
+            button2.Font = new Font("Times New Roman", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button2.ForeColor = Color.White;
+            button2.Location = new Point(39, 468);
+            button2.Name = "button2";
+            button2.Size = new Size(526, 87);
+            button2.TabIndex = 3;
+            button2.Text = "Login";
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.DarkSlateGray;
+            button1.Font = new Font("Times New Roman", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button1.ForeColor = Color.White;
+            button1.Location = new Point(39, 184);
+            button1.Name = "button1";
+            button1.Size = new Size(526, 87);
+            button1.TabIndex = 2;
+            button1.Text = "Company";
+            button1.UseVisualStyleBackColor = false;
+            button1.Click += button1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Times New Roman", 63.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(39, 59);
+            label2.Name = "label2";
+            label2.Size = new Size(544, 96);
+            label2.TabIndex = 1;
+            label2.Text = "Experience++";
+            // 
+            // panel2
+            // 
+            panel2.BackColor = Color.Transparent;
+            panel2.BackgroundImage = (Image)resources.GetObject("panel2.BackgroundImage");
+            panel2.ForeColor = Color.IndianRed;
+            panel2.Location = new Point(1078, 424);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(59, 47);
+            panel2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Times New Roman", 63.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.Transparent;
+            label3.Location = new Point(830, 483);
+            label3.Name = "label3";
+            label3.Size = new Size(544, 96);
+            label3.TabIndex = 3;
+            label3.Text = "Experience++";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Times New Roman", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.ForeColor = Color.Transparent;
+            label8.Location = new Point(274, 662);
+            label8.Name = "label8";
+            label8.Size = new Size(55, 31);
+            label8.TabIndex = 10;
+            label8.Text = "OR";
+            // 
+            // panel3
+            // 
+            panel3.BackgroundImage = (Image)resources.GetObject("panel3.BackgroundImage");
+            panel3.Location = new Point(67, 714);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(474, 95);
+            panel3.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackgroundImage = (Image)resources.GetObject("$this.BackgroundImage");
+            ClientSize = new Size(1904, 1041);
+            Controls.Add(label3);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Name = "Form1";
+            Text = "Login";
+            Load += Form1_Load;
+            panel1.ResumeLayout(false);
+            panel1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Panel panel1;
+        private Label label2;
+        private Panel panel2;
+        private Button button1;
+        private Label label3;
+        private TextBox textBox2;
+        private TextBox textBox1;
+        private Button button2;
+        private Label label4;
+        private Label label6;
+        private Label label5;
+        private Label label7;
+        private Label label8;
+        private Panel panel3;
+    }
+}
